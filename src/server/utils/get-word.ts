85aa83f8b5bfc2fb83fa20a681a69d13f34a026d
alex.curtin <alexhcurtin@gmail.com>
2023-08-27 16:35:29 -0400

    fix ts error

--- src/server/utils/get-word.ts
+++ src/server/utils/get-word.ts
@@ -1,6 +1,6 @@
 import { wordList } from "./word-list";
 
-export const getWordOfTheDay: string = () => {
+export const getWordOfTheDay = (): string => {
 	const date = new Date();
 	const day = date.getDate();
 	return wordList[day - 1];
