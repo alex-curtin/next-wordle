631445447b3b4f22ec20f1603d4f2f22549fca5e
alex.curtin <alexhcurtin@gmail.com>
2023-08-28 11:27:11 -0400

    change font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Libre_Franklin } from "next/font/google";
 
 import { api } from "~/utils/api";
 import "./animation.css";
 
+const libreFranklin = Libre_Franklin({ subsets: ["latin"] });
+
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
-		<>
+		<div className={libreFranklin.className}>
 			<Toaster position="top-center" />
 			<Component {...pageProps} />
-		</>
+		</div>
 	);
 };
 
